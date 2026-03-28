755da3d0f484779689b25c7cb7490f547bf703d2
bachiratmani1313-spec <bachiratmani1313@gmail.com>
2026-03-28 22:15:49 +0100

    Remove unused widget divs from page
    Removed unused widget divs for Aviasales and HotelLook.

--- app/page.js
+++ app/page.js
@@ -9,8 +9,6 @@ export default function Home() {
 
       {/* AVIASALES */}
       <div className="widget">
-        <div id="aviasales-widget"></div>
-
         <Script
           strategy="lazyOnload"
           src="https://tp.media/content?currency=eur&locale=fr&marker=499802&trs=0&powered_by=true&searchUrl=www.aviasales.com/search&theme=dark&border_radius=10&plain=false&color_button=%23ffd700&color_icons=%23ffd700&color_background=%230a0a0a&color_text=%23ffffff&color_border=%23ffd700&promo_id=7873&campaign_id=100"
@@ -19,8 +17,6 @@ export default function Home() {
 
       {/* HOTELLOOK */}
       <div className="widget">
-        <div id="hotellook-widget"></div>
-
         <Script
           strategy="lazyOnload"
           src="https://tp.media/content?currency=eur&locale=fr&marker=499802&trs=0&powered_by=true&searchUrl=search.hotellook.com&theme=dark&border_radius=10&plain=false&color_button=%23ffd700&color_icons=%23ffd700&color_background=%230a0a0a&color_text=%23ffffff&color_border=%23ffd700&promo_id=7873&campaign_id=101"
@@ -32,7 +28,6 @@ export default function Home() {
         <a
           href="https://kiwi.tp.st/499802"
           target="_blank"
-          style={{ color: "#ffd700" }}
         >
           ✈️ Find cheaper flights (Kiwi)
         </a>
