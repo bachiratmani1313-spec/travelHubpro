9f471de47ddd4e86a7d592e80396bdedd7d7a5dd
bachiratmani1313-spec <bachiratmani1313@gmail.com>
2026-03-28 22:07:37 +0100

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main className="container">
+      <h2>Compare Flights & Hotels</h2>
+
+      <div className="blocks">
+        <div className="card">✈️ Flights (Aviasales / Kiwi)</div>
+        <div className="card">🏨 Hotels (Booking / Hotellook)</div>
+        <div className="card">🚗 Cars (soon)</div>
+      </div>
+    </main>
+  );
+}
